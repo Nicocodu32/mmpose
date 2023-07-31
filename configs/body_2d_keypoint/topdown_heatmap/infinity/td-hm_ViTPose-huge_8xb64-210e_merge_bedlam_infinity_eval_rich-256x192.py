5a3aedd9d4c48e18871ce998611b73b32f1c67c1
yonigozlan <yoni.gozlan@student-cs.fr>
2023-07-31 01:04:17 -0700

    add val_evaluators to vit huge

--- configs/body_2d_keypoint/topdown_heatmap/infinity/td-hm_ViTPose-huge_8xb64-210e_merge_bedlam_infinity_eval_rich-256x192.py
+++ configs/body_2d_keypoint/topdown_heatmap/infinity/td-hm_ViTPose-huge_8xb64-210e_merge_bedlam_infinity_eval_rich-256x192.py
@@ -182,6 +182,27 @@ val_dataloader = dict(
 )
 test_dataloader = val_dataloader
 
+# evaluators
+val_evaluator = [
+    dict(
+        type="InfinityMetric",
+        ann_file=data_root + "val_annotations.json",
+        use_area=False,
+    ),
+    dict(
+        type="InfinityCocoMetric",
+        ann_file=data_root + "val_annotations.json",
+        use_area=False,
+    ),
+    dict(
+        type="InfinityAnatomicalMetric",
+        ann_file=data_root + "val_annotations.json",
+        use_area=False,
+    ),
+]
+
+test_evaluator = val_evaluator
+
 
 # visualizer
 vis_backends = [
